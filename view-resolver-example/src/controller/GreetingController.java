f982460b6bd1e9913460d27aca259ca8bb64b6a5
Pato <pato24193@gmail.com>
2021-02-18 14:31:25 +0700

    pato add

---
+++ view-resolver-example/src/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Các bạn có thể tìm hiểu thêm  phần này tại blogs: https://levunguyen.com,
+ * blogs chuyên về lập trình và các kỹ năng mềm trong nghề lập trình
+ */
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String showGreeting(Model model) {
+        model.addAttribute("helloViewResolverString", "Hello View Resolver!");
+        return "greeting";
+    }
+}
---
+++ view-resolver-example/web/WEB-INF/views/greeting.jsp
@@ -0,0 +1,13 @@
+<%--
+    Các bạn có thể tìm hiểu thêm  phần này tại blogs: https://levunguyen.com,
+    blogs chuyên về lập trình và các kỹ năng mềm trong nghề lập trình
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello View Resolver</title>
+</head>
+<body>
+    <h2>${helloViewResolverString}</h2>
+</body>
+</html>
